feda9a5e9290806d8789c09ad7c548a8a8544ab5
fernandes-draw <fernandes_draw@hotmail.com>
2025-10-23 21:16:29 -0300

    Theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "./App.css";
+
+const theme = createTheme({
+  focusRing: "never",
+  fontFamily: "Poppins, sans-serif",
+  headings: { fontFamily: "Merriweather, serif" },
+  colors: {
+    primary: [
+      "#f1fcfa",
+      "#cff8ef",
+      "#9ff0e1",
+      "#67e1cf",
+      "#32b9a9",
+      "#1fad9f",
+      "#168b82",
+      "#166f69",
+      "#165955",
+      "#174a47",
+      "#072c2b",
+    ],
+    neutral: [
+      "#f6f6f6",
+      "#e7e7e7",
+      "#d1d1d1",
+      "#b0b0b0",
+      "#888888",
+      "#6d6d6d",
+      "#5d5d5d",
+      "#4f4f4f4",
+      "#454545",
+      "#3d3d3d",
+      "000000",
+    ],
+  },
+  primaryColor: "primary",
+  primaryShade: 4,
+  defaultGradient: {
+    from: "primary.4",
+    to: "primary.8",
+    deg: 132,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider theme={theme}>
+      <div>
+        <Button variant="gradient">Hello</Button>
+        <span>a</span>
+      </div>
+    </MantineProvider>
   );
 }
 
